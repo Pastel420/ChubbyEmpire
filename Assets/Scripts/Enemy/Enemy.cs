8beea2cf0799e9a25783b84cd6470cdb756f7e89
hecc <1652607659@qq.com>
2026-02-04 02:11:22 +0800

    Enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Animator anim;
+    public PhysicsCheck physicsCheck;
+    [Header("基本参数")]
+
+    public float NormalSpeed;
+    public float ChaseSpeed;
+    public float CurrentSpeed;
+    public Vector3 faceDir;
+    public float hurtForce;
+    public Transform attacker;
+
+    [Header("检测")]
+    public Vector2 centerOffset;
+    public Vector2 checksize;
+    public LayerMask attackLayer;
+    public float CheckDistance;
+
+    [Header("计时器")]
+    public float waitTime;
+    public float waitTimeCounter;
+    public bool wait;
+    public float lostTimeCounter;
+    public float lostTime;
+
+    [Header("状态")]
+    public bool isHurt;
+    public bool isDead;
+
+    protected BaseState currentState;
+    protected BaseState patrolState;
+    protected BaseState chaseState;
+
+    protected virtual void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        CurrentSpeed = NormalSpeed;
+        physicsCheck= GetComponent<PhysicsCheck>();
+        waitTimeCounter = waitTime;
+    }
+
+    private void OnEnable()
+    {
+        currentState = patrolState;
+        currentState.OnEnter(this);
+    }
+
+    public void Update()
+    {
+        faceDir=new Vector3(-transform.localScale.x, 0, 0);
+
+        currentState.LogicUpdate();
+        TimeCounter();
+    }
+
+    public void FixedUpdate()
+    {
+        if (!isHurt && !isDead && !wait)
+        {
+            Move();
+        }
+        currentState.PhysicsUpdate();
+    }
+
+    private void OnDisable()
+    {
+        currentState.OnExit();
+    }
+
+    public virtual void Move()
+    {
+        rb.velocity = new Vector2(CurrentSpeed * faceDir.x*Time.deltaTime, rb.velocity.y);
+    }
+
+    public bool FoundPlayer()
+    {
+        return Physics2D.BoxCast(transform.position + (Vector3)centerOffset, checksize, 0, faceDir, CheckDistance, attackLayer);
+        
+    }
+
+    public void SwitchState(NPCState state)
+    {
+        var newState = state switch
+        {
+            NPCState.Patrol => patrolState,
+            NPCState.Chase => chaseState,
+            _ => null
+        };
+        currentState.OnExit();
+        currentState = newState;
+        currentState.OnEnter(this);
+    }
+
+    public void TimeCounter()
+    {
+        if (wait)
+        {
+            waitTimeCounter -= Time.deltaTime;
+            if (waitTimeCounter <= 0)
+            {
+                wait = false;
+                waitTimeCounter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        }
+
+        if(!FoundPlayer()&&lostTimeCounter>0)
+        {
+            lostTimeCounter -= Time.deltaTime;
+        }
+    }
+
+    public void OnTakeDamage(Transform attackTrans)
+    {
+        attacker= attackTrans;
+        //Turn
+        if(attackTrans.position.x>transform.position.x)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if(attackTrans.position.x<transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        //Hurt
+        isHurt= true;
+        anim.SetTrigger("hurt");
+        Vector2 dir=new Vector2(transform.position.x - attackTrans.position.x,0).normalized;
+        rb.velocity= new Vector2(0,rb.velocity.y);
+        StartCoroutine(Onhurt(dir));
+    }
+
+    private IEnumerator Onhurt(Vector2 dir)
+    {
+        rb.AddForce(dir*hurtForce,ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.5f);
+        isHurt= false;
+    }
+
+    public void OnDie()
+    {
+        gameObject.layer = 2;
+        anim.SetBool("dead", true);
+        isDead= true;
+    }
+
+    public void DestroyAfterAnimation()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position + (Vector3)centerOffset + new Vector3(CheckDistance* -transform.localScale.x,0),0.2f);
+    }
+}
--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -4,25 +4,50 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    private CapsuleCollider2D coll;
+
     [Header("¼ì²â²ÎÊý")]
+    public bool manual;
+
     public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
     public float checkRaduis;
     public LayerMask groundLayer;
 
     [Header("×´Ì¬")]
     public bool isGround;
+    public bool touchLeftWall;
+    public bool touchRightWall;
 
+    private void Start()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+
+        if(!manual)
+        {
+            rightOffset=new Vector2((coll.bounds.size.x+coll.offset.x)/2, coll.bounds.size.y/2);
+            leftOffset=new Vector2(-(coll.bounds.size.x+coll.offset.x)/2, coll.bounds.size.y/2);
+        }
+    }
     private void Update()
     {
         Check();
     }
     public void Check()
     {
+        //CheckGround
         isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRaduis, groundLayer);
+        //CheckWall
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRaduis, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRaduis, groundLayer);
+
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRaduis);
     }
 }
