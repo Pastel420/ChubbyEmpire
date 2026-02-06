c7344ffb4d8cccc55231e556759eae761600d14a
Pastel420 <t6004125112@163.com>
2026-02-06 20:12:32 +0800

    场景搭建版

---
+++ Assets/Scripts/General/Chest.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour, Iinteractable
+{
+    public void TriggerAction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
---
+++ Assets/Scripts/Utilities/Iinteractable.cs
@@ -0,0 +1,5 @@
+
+public interface Iinteractable 
+{
+    void TriggerAction();
+}
